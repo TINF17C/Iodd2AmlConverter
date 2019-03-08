5c50f8f21ca15e716d22fff8a1a3ff6538fbfc24
pa6ne6 <pascal.nerger@it-designers.de>
2019-03-08 18:24:38 +0100

    #3: Updated DocumentInfoRule

---
+++ AMLRider.Library/src/Rules/DataObjects/DocumentInfoObj.cs
@@ -0,0 +1,15 @@
+namespace AMLRider.Library.Rules.DataObjects
+{
+    
+    public class DocumentInfoObj
+    {
+        
+        public string Copyright { get; set; }
+        
+        public string ReleaseDate { get; set; }
+        
+        public string Version { get; set; }
+        
+    }
+    
+}
--- AMLRider.Library/src/Rules/DocumentInfoRule.cs
+++ AMLRider.Library/src/Rules/DocumentInfoRule.cs
@@ -1,49 +1,70 @@
 using System;
 using System.Xml;
 using System.Xml.Linq;
+using AMLRider.Library.Extensions;
 using AMLRider.Library.Helpers;
+using AMLRider.Library.Rules.DataObjects;
 
 namespace AMLRider.Library.Rules
 {
     
+    /// <inheritdoc />
+    /// <summary>
+    /// Responsible for parsing the DocumentInfo tag.
+    /// </summary>
     public class DocumentInfoRule : IConversionRule
     {
 
-        private XElement CreateWriterHeader(XElement element)
+        /// <summary>
+        /// Stores the attribute values of an <see cref="XElement"/> in a <see cref="DocumentInfoObj"/>.
+        /// </summary>
+        /// <param name="element">The element to parse.</param>
+        /// <returns>The parsed <see cref="DocumentInfoObj"/>.</returns>
+        private static DocumentInfoObj GetDocumentInfo(XElement element)
         {
-            var writerHeader = new XElement("WriterHeader");
+            return new DocumentInfoObj()
+            {
+                Copyright = element.GetAttributeValue("copyright"),
+                ReleaseDate = element.GetAttributeValue("releaseDate"),
+                Version = element.GetAttributeValue("version")
+            };
+        }
+        
+        /// <summary>
+        /// Creates the AdditionalInformationHeader element.
+        /// </summary>
+        /// <param name="obj">The <see cref="DocumentInfoObj"/> providing the necessary information.</param>
+        /// <returns>The created <see cref="XElement"/>.</returns>
+        private static XElement CreateInfoHeader(DocumentInfoObj obj)
+        {
+            var infoHeader = XmlHelper.CreateElement("AdditionalInformation");
+            var writerHeader = XmlHelper.CreateElement(infoHeader, "WriterHeader");
 
-            var writerName = XmlHelper.CreateElementWithValue("WriterName", "AMLRider");
-            writerHeader.Add(writerName);
+            XmlHelper.CreateElement(writerHeader, "WriterName", "AMLRider");
+            XmlHelper.CreateElement(writerHeader, "WriterID", Guid.NewGuid().ToString());
+            XmlHelper.CreateElement(writerHeader, "WriterVendor", obj.Copyright);
+            XmlHelper.CreateElement(writerHeader, "WriterVendorURL");
+            XmlHelper.CreateElement(writerHeader, "WriterVersion", obj.Version);
+            XmlHelper.CreateElement(writerHeader, "WriterRelease", obj.ReleaseDate);
+            XmlHelper.CreateElement(writerHeader, "LastWritingDateTime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
 
-            var guid = Guid.NewGuid().ToString();
-            var writerId = XmlHelper.CreateElementWithValue("WriterID", guid);
-            writerHeader.Add(writerId);
-
-            var copyrightText = GetCopyrightText(element);
-            var writerVendor = XmlHelper.CreateElementWithValue("WriterVendor", copyrightText);
-            writerHeader.Add(writerVendor);
-
-            return writerHeader;
+            return infoHeader;
         }
 
-        private string GetCopyrightText(XElement element)
-        {
-            var attribute = element.Attribute("copyright");
-            return attribute?.Value;
-        }
-
+        /// <inheritdoc />
         public bool CanApplyRule(XElement element)
         {
             return element.Name == "DocumentInfo";
         }
 
+        /// <inheritdoc />
         public XElement Apply(XElement element)
         {
             if(!CanApplyRule(element))
                 throw new InvalidOperationException("The given node is not a <DocumentInfo> node.");
 
-            return CreateWriterHeader(element);
+            var documentInfo = GetDocumentInfo(element);
+            return CreateInfoHeader(documentInfo);
         }
         
     }
