8a9e02815eda55148a8e7b3f18b69ac9b9d73213
pa6ne6 <pascal.nerger@it-designers.de>
2019-03-08 09:46:26 +0100

    Added project files

---
+++ AMLRider/src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AMLRider
+{
+    
+    public static class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+    
+}
