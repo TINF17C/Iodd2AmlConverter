4e1be99a99ed1bde76493d3cdf0db4dcd741c013
Yannik Schöberl <inf17031@lehre.dhbw-stuttgart.de>
2019-03-12 10:51:48 +0100

    #2: Mock interface was implemented.

---
+++ AMLRider.Library/src/IConversionHandler.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AMLRider.Library
+{
+    /// <summary>
+    /// Responsible for converting IODD files into AML files.
+    /// </summary>
+    public interface IConversionHandler
+    {
+        /// <summary>
+        /// Converts an IODD file into an AML file.
+        /// </summary>
+        /// <param name="ioddFileData">The content of an IODD file as a string.</param>
+        /// <returns>Returns a string which contains the generated AML data.</returns>
+        string Convert(string ioddFileData);
+
+    }
+}
